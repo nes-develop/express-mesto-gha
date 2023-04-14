94acca12cbfef88b254c8fca3f9e8c8ff6c10f2f
Aleksey Nazarov <nazarovaleksey@list.ru>
2023-04-14 23:42:23 +0300

    fix after 2 review

--- errors/allErrors.js
+++ errors/allErrors.js
@@ -2,10 +2,12 @@ const ForbiddenError = require('./ForbiddenError');
 const NotFound = require('./NotFound');
 const UnauthorizedError = require('./UnauthorizedError');
 const ValidationError = require('./ValidationError');
+const ConflictError = require('./ConflictError');
 
 module.exports = {
   ForbiddenError,
   NotFound,
   UnauthorizedError,
   ValidationError,
+  ConflictError,
 };
