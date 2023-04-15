7cd10861469dbdc887281e4010448ebdb0471268
nes-develop <nazarovaleksey@list.ru>
2023-04-15 15:22:27 +0300

    fix after review

--- errors/allErrors.js
+++ errors/allErrors.js
@@ -2,12 +2,10 @@ const ForbiddenError = require('./ForbiddenError');
 const NotFound = require('./NotFound');
 const UnauthorizedError = require('./UnauthorizedError');
 const ValidationError = require('./ValidationError');
-const ConflictError = require('./ConflictError');
 
 module.exports = {
   ForbiddenError,
   NotFound,
   UnauthorizedError,
   ValidationError,
-  ConflictError,
 };
